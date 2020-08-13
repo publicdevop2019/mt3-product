b4dc6b408607e44dbff67478587d5600e3789fe1
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-08-13 16:52:12 -0400

    work snapshot

--- src/main/java/com/hw/aggregate/catalog/representation/AdminCatalogCardRep.java
+++ src/main/java/com/hw/aggregate/catalog/representation/AdminCatalogCardRep.java
@@ -10,8 +10,10 @@ public class AdminCatalogCardRep {
     private Long id;
     private String name;
     private Long parentId;
+    public transient static final String ADMIN_REP_PARENT_ID_LITERAL = "parentId";
     private Set<String> attributes;
     private Catalog.CatalogType catalogType;
+    public transient static final String ADMIN_REP_CATALOG_TYPE_LITERAL = "catalogType";
 
     public AdminCatalogCardRep(Catalog catalog) {
         this.id = catalog.getId();
