7a922681a429e605916fced03776d68dea448820
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-09-12 21:17:46 -0400

    remove cache

--- src/main/java/com/hw/shared/cache/RedisETagCacheRepo.java
+++
@@ -1,8 +0,0 @@
-package com.hw.shared.cache;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RedisETagCacheRepo extends CrudRepository<String, String> {
-}
